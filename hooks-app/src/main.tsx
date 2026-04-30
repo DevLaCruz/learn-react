02075434f4b5b44be2cee164b1a849ab4a518705
DevLaCruz <alejantony.22@hotmail.com>
2026-04-30 12:45:38 -0500

    add shadcn

--- hooks-app/src/main.tsx
+++ hooks-app/src/main.tsx
@@ -5,8 +5,9 @@ import './index.css'
 // import { TrafficLight } from './01-useState/TrafficLight'
 // import { TrafficLightWithEffect } from './02-useEffect/TrafficLightWithEffect'
 // import { TrafficLightWithHook } from './02-useEffect/TrafficLightWithHook'
-//import { PokemonPage } from './03-examples/PokemonPage'
-import { FocusScreen } from './04-useRef/FocusScreen'
+// import { PokemonPage } from './03-examples/PokemonPage'
+// import { FocusScreen } from './04-useRef/FocusScreen'
+import { TasksApp } from './05-useReducer/TaskApp'
 
 
 createRoot(document.getElementById('root')!).render(
@@ -17,7 +18,8 @@ createRoot(document.getElementById('root')!).render(
     {/* <TrafficLightWithEffect/> */}
     {/* <TrafficLightWithHook/> */}
     {/* <PokemonPage/> */}
-    <FocusScreen/>
+    {/* <FocusScreen/> */}
+    <TasksApp/>
 
 
   </StrictMode>
