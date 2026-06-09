b7ee993af488ad2615d2924a177dc84aa5a2858f
DevLaCruz <alejantony.22@hotmail.com>
2026-06-08 19:56:18 -0500

    create auth context

--- hooks-app/src/09-useContext/ProfessionalApp.tsx
+++ hooks-app/src/09-useContext/ProfessionalApp.tsx
@@ -2,6 +2,8 @@ import { RouterProvider } from 'react-router'
 import {appRouter} from './router/app.router'
 import { UserContextProvider } from './context/UserContext'
 
+// Aca vamos a colocar idealmente nuestro contexto
+
 export const ProfessionalApp = () => {
   return (
     <UserContextProvider >
--- hooks-app/src/09-useContext/context/UserContext.tsx
+++ hooks-app/src/09-useContext/context/UserContext.tsx
@@ -1,22 +1,71 @@
-import { useState } from "react"
+import { createContext, useState, type PropsWithChildren } from "react"
+import type { User } from "../data/user-mock.data"
+
+// esta tambien es una manera de tipar el children:s
+// interface UserContextProps {
+//     children: React.ReactNode // hay varias formas de tipar esto
+// }
+
+
+type AuthStatus = 'checking' | 'suthenticated' | 'not-authenticated'
 
 interface UserContextProps {
-    children: React.ReactNode
+    // state
+    authStatus: AuthStatus
+    user: User | null
+
+
+    // methods
+    login: (userId: number) => boolean
+    logout: () => void
 }
 
-export const UserContextProvider = ({children}: any) => {
+//Inicializando contexto
+export const UserContext = createContext({} as UserContextProps)
 
-const [name, setName] = useState('Alejandro')
 
+export const UserContextProvider = ({children}: PropsWithChildren) => {
+// Recordar que un Children es una prop especial que representa los elementos anidados de un componente
+
+// Un provider es un High older component que puede proveer usualmente algun tipo de estado o funcionalidad a todos sus hijos, y sus hijos pueden
+// navegar hacia el de diversas maneras, una de ellas es con un hook como useContext, sin embargo en nuevas versiones lo ideal es usar el "use"
+
+
+const [authStatus, setAuthStatus] = useState<AuthStatus>('checking')
+const [user, setUser] = useState<User|null>(null)
+
+const handleLogin = (userId: number) => {
+    console.log(userId);   
+    return true
+}
+
+const handleLogout = () => {
+    console.log('logout');
+    
+}
+
+
+// La idea es que en algun Provider proveer información y comportamientos (funciones), entonces ara quelos componentes puedan compartir un 
+// estado global debemos colocarlo en un punto alto de todo inclusive mas arriba del router, ese lugar es un Contexto y por eso hemos creado 
+// este functional component
 
 // El tipado ayuda a saber que informacion es la que fluye
-// por aca podemos reotrnar una estrcutura html, sin embargo, no se recomienda que en el UserContextProvider se retorna un html
+// por aca podemos retornar una estrcutura html, sin embargo, no se recomienda que en el UserContextProvider se retorna un html
 // sería mejor para eso crearse un componente aparte que herede esa estructura html a sus hijos , el provider es mas ara lógica de negocio
 
-return (
-<div>
-    <h1>Hello</h1>
-    {children}
-</div>
-)
+
+// return (
+// <div>
+//     <h1>Hello</h1>
+//     {children}
+// </div>
+// )
+
+return <UserContext value={{
+    authStatus: authStatus,
+    user:null,
+    login: handleLogin,
+    logout: handleLogout
+}}>{children}</UserContext>
+
 }
