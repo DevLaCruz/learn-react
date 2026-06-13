1e2d0824842e4d266704fd9f1887a0367b5defe8
DevLaCruz <alejantony.22@hotmail.com>
2026-06-12 23:20:32 -0500

    add the breadcumb

--- spa-project/src/router/app.router.tsx
+++ spa-project/src/router/app.router.tsx
@@ -1,24 +1,44 @@
-import { AdminPage } from "@/admin/pages/AdminPage";
-import { HeroPage } from "@/heroes/pages/hero/HeroPage";
-import { HomePage } from "@/heroes/pages/home/HomePage";
-import { createBrowserRouter } from "react-router";
+import { lazy } from 'react';
+import { createBrowserRouter } from 'react-router';
 
-export const router = createBrowserRouter([
-    {
-        path: '/',
-        element: <HomePage/>
-    },
+import { AdminLayout } from '@/admin/layouts/AdminLayout';
+import { AdminPage } from '@/admin/pages/AdminPage';
+import { HeroesLayout } from '@/heroes/layouts/HeroesLayout';
+import { HeroPage } from '@/heroes/pages/hero/HeroPage';
+import { HomePage } from '@/heroes/pages/home/HomePage';
 
-    {
-        path: '/heroes/1',
-        element: <HeroPage/>
-    },
-    {
-        path: '/',
-        element: <HomePage/>
-    },
-    {
-        path: '/admin',
-        element: <AdminPage/>
-    }
-])
+// import { SearchPage } from '@/heroes/pages/search/SearchPage';
+
+const SearchPage = lazy(() => import('@/heroes/pages/search/SearchPage'));
+
+export const appRouter = createBrowserRouter([
+  {
+    path: '/',
+    element: <HeroesLayout />,
+    children: [
+      {
+        index: true,
+        element: <HomePage />,
+      },
+      {
+        path: 'heroes/1',
+        element: <HeroPage />,
+      },
+      {
+        path: 'search',
+        element: <SearchPage />,
+      },
+    ],
+  },
+
+  {
+    path: '/admin',
+    element: <AdminLayout />,
+    children: [
+      {
+        index: true,
+        element: <AdminPage />,
+      },
+    ],
+  },
+]);
